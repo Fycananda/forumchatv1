227a25d31f5675a113091dc3c10687ea7026ae74
FAA <febyacdistiaananda@gmail.com>
2025-10-09 00:07:23 +0700

    first commit

---
+++ forumchat-backend/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
+const JWT_SECRET = process.env.JWT_SECRET || "defaultsecret";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect Mongo
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+// Schema User
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+const User = mongoose.model("User", UserSchema);
+
+// Signup
+app.post("/signup", async (req, res) => {
+    const { username, password } = req.body;
+    const hashed = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashed });
+    await user.save();
+    res.json({ message: "User created" });
+});
+
+// Login
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Wrong password" });
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
+    res.json({ token });
+});
+
+// Socket.IO
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+    console.log("user connected", socket.id);
+
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+});
+
+httpServer.listen(4000, () => console.log("Server with socket.io running"));
